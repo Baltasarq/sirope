366d389dab5dbc6f715517b68bcf1c4b02b78b05
baltasarq <baltasarq@gmail.com>
2022-03-24 19:18:41 +0100

    Bumped setup version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 setup(
     name = 'sirope',
     packages = ['sirope'],
-    version = '0.1.1',
+    version = '0.1.2',
     license='MIT',
     description = 'Simple Interface for Redis of Object Persistence',
     author = 'Baltasar',
