63df6e3ba5451bb9fadded7675ea3b113006be03
baltasar <baltasarq@gmail.com>
2022-04-03 13:26:04 +0200

    Bumps version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 setup(
     name = 'sirope',
     packages = ['sirope'],
-    version = '0.1.3',
+    version = '0.1.4',
     license='MIT',
     description = 'Simple Interface for Redis of Object Persistence',
     author = 'Baltasar',
